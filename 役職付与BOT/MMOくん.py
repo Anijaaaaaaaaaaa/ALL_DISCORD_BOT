87c5490e8568cf794b2e71742cc60e35f5b2e7f1
The.First.Step <46297678+Anijaaaaaaaaaaa@users.noreply.github.com>
2020-01-29 17:21:10 +0900

    Update MMOくん.py

--- 役職付与BOT/MMOくん.py
+++ 役職付与BOT/MMOくん.py
@@ -1,8 +1,9 @@
-import asyncio
+import traceback
 import discord
 import random
 import aiomysql
 import json
+import asyncio
 from discord.ext import commands
 
 with open(r'setting.json', mode='r', encoding='utf-8') as fh:
@@ -10,6 +11,7 @@ with open(r'setting.json', mode='r', encoding='utf-8') as fh:
     json_txt = str(json_txt).replace("'", '"').replace('True', 'true').replace('False', 'false')
     token = json.loads(json_txt)['token']
     prefix = json.loads(json_txt)['prefix']
+loop = asyncio.new_event_loop()
 on_ready_complete = []
 
 
@@ -23,20 +25,23 @@ async def run():
 
 class MyBot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix=commands.when_mentioned_or(prefix))
+        super().__init__(command_prefix=commands.when_mentioned_or(prefix), loop=loop)
         self.remove_command('help')
 
     async def on_ready(self):
         for cog in ['mmo.mmo_bot', 'mmo.main']:
-            self.load_extension(cog)
+            try:
+                self.load_extension(cog)
+            except commands.ExtensionAlreadyLoaded:
+                pass
 
-        await self.change_presence(activity=discord.Game(name=f"{prefix}help"))
+        await self.change_presence(activity=discord.Game(name=f"{prefix}help | {len(self.guilds)}guilds", type=1))
         on_ready_complete.append("ok")
-        await self.get_channel(550674420222394378).edit(name=f"総メッセージ数: 5708501")
+        await self.get_channel(550674420222394378).edit(name=f"総メッセージ数: 7183026")
 
     async def on_member_join(self, member):
         if not member.guild.id == 337524390155780107 or self.user == member:
-            return        
+            return
         up = discord.Color(random.randint(0, 0xFFFFFF))
         embed = discord.Embed(title=f"{member.name}さんがこの鯖に入りました！～", description=f"現在の鯖の人数: `{len(member.guild.members)}人`\n{member.mention}さんに役職を付与しました。", color=up)
         embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/{0.id}/{0.avatar}.png?size=1024".format(member))
@@ -50,8 +55,10 @@ class MyBot(commands.Bot):
                 await cur.execute("SELECT * FROM get_role WHERE author_id=%s;", (member.id,))
                 for row in await cur.fetchall():
                     role = discord.utils.get(member.guild.roles, id=row[1])
-                    await member.add_roles(role)
-
+                    try:
+                        await member.add_roles(role)
+                    except Exception as _:
+                        pass
                 await cur.execute("delete from get_role where author_id=%s;", (member.id,))
                 await conn.commit()
 
@@ -72,24 +79,30 @@ class MyBot(commands.Bot):
                         await cur.execute("INSERT INTO get_role VALUES(%s,%s);", (member.id, role.id))
                         await conn.commit()
 
-    async def on_command_error(self, ctx, error):
+    async def on_command_error(self, ctx, error1):
         if not on_ready_complete:
             return await ctx.send(f"{ctx.message.author.mention}\n```『{self.user.name}』は現在再起動中.....```")
         try:
-            if isinstance(error, commands.CommandNotFound):
+            if isinstance(error1, commands.CommandNotFound):
                 msg = discord.Embed(description=f"{ctx.message.author.mention}さん\n{ctx.message.content}というコマンドはありません！", color=0xC41415)
                 return await ctx.send(embed=msg)
-            elif isinstance(error, commands.CommandOnCooldown):
-                msg = discord.Embed(description='{}さん！\nこのコマンドは{:.2f}秒後に使用可能です！'.format(ctx.message.author.mention, error.retry_after), color=0xC41415)
+            elif isinstance(error1, commands.CommandOnCooldown):
+                msg = discord.Embed(description='{}さん！\nこのコマンドは{:.2f}秒後に使用可能です！'.format(ctx.message.author.mention, error1.retry_after), color=0xC41415)
                 return await ctx.send(embed=msg)
-            elif isinstance(error, commands.MissingPermissions):
+            elif isinstance(error1, commands.MissingPermissions):
                 msg = discord.Embed(description=f"{ctx.message.author.mention}さん\nあなたはこのコマンドを使用するには権限がありません！", color=0xC41415)
                 return await ctx.send(embed=msg)
-            
+
         except discord.Forbidden:
             pass
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
+    try:
+        print("ready!")
+        main_task = loop.create_task(run())
+        loop.run_until_complete(main_task)
+        loop.close()
+
+    except Exception as error:
+        print("エラー情報\n" + traceback.format_exc())
