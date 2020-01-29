b8325613b8469058a7cbda264162cd9f2841c8b6
The.First.Step <46297678+Anijaaaaaaaaaaa@users.noreply.github.com>
2020-01-29 17:23:56 +0900

    Update chase.py

--- message_chase/chase.py
+++ message_chase/chase.py
@@ -1,8 +1,10 @@
 import json
 import discord
+import traceback
 import asyncio
 from discord.ext import commands
 
+loop = asyncio.new_event_loop()
 with open(r'setting.json', mode='r', encoding='utf-8') as fh:
     json_txt = fh.read()
     json_txt = str(json_txt).replace("'", '"').replace('True', 'true').replace('False', 'false')
@@ -11,16 +13,22 @@ with open(r'setting.json', mode='r', encoding='utf-8') as fh:
 
 
 async def run():
-    bot = MyBot()
     try:
-        await bot.start(token)
-    except KeyboardInterrupt:
-        await bot.logout()
+        bot = MyBot()
+        try:
+            await bot.start(token)
+        except KeyboardInterrupt:
+            await bot.logout()
+            await bot.close()
+            await bot.clear()
+
+    except Exception as _:
+        print("エラー情報\n" + traceback.format_exc())
 
 
 class MyBot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix=commands.when_mentioned_or(prefix))
+        super().__init__(command_prefix=commands.when_mentioned_or(prefix), loop=loop)
         self.remove_command('help')
 
     async def on_ready(self):
@@ -28,9 +36,41 @@ class MyBot(commands.Bot):
             self.load_extension('cogs.help')
         except commands.errors.ExtensionAlreadyLoaded:
             pass
-        await self.change_presence(activity=discord.Game(name="mc!help"))
+
+        await self.change_presence(activity=discord.Game(name=f"{prefix}help | {len(self.guilds)}guilds", type=1))
+
+    async def on_command_error(self, ctx, error1):
+        if isinstance(error1, (commands.CommandNotFound, commands.CommandInvokeError, commands.BadArgument, commands.UnexpectedQuoteError, commands.CommandOnCooldown, commands.MissingPermissions, commands.MissingRequiredArgument)):
+            return
+
+        elif isinstance(error1, commands.BotMissingPermissions):
+            permission = {'read_messages': "read_messages", 'send_messages': "send_messages", 'read_message_history': "read_message_history", 'embed_links': "embed_links"}
+            text = ""
+            for all_error_permission in error1.missing_perms:
+                text += f"❌:{permission[all_error_permission]}\n"
+                del permission[all_error_permission]
+
+            for all_arrow_permission in list(permission.values()):
+                text += f"✅:{all_arrow_permission}\n"
+
+            embed = discord.Embed(description=text)
+            return await ctx.message.author.send(embed=embed)
+        else:
+            print("エラー情報\n" + traceback.format_exc())
+    
+    async def on_guild_join(self, _):
+        await self.change_presence(activity=discord.Game(name=f"{prefix}help | {len(self.guilds)}guilds", type=1))
+    
+    async def on_guild_remove(self, _):
+        await self.change_presence(activity=discord.Game(name=f"{prefix}help | {len(self.guilds)}guilds", type=1))
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
+    try:
+        print("ready!")
+        main_task = loop.create_task(run())
+        loop.run_until_complete(main_task)
+        loop.close()
+
+    except Exception as error:
+        print("エラー情報\n" + traceback.format_exc())
